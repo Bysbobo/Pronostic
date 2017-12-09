025ddb2e0d5164e629e09f0f6c6b48a1d0c1d439
Bysbobo <boixpierre@gmail.com>
2017-12-09 18:54:03 +0100

    End of correction for new object display

--- src/newmatchwindow.cpp
+++
@@ -1,6 +0,0 @@
-/********************************************
-    * File Name :   "addmatchwidget.cpp"
-    * Authors :     P. Boix and E. Moussy
-    * Created on :  09/12/2017
-********************************************/
-
