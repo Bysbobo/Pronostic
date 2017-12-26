9c2ccfb140dc79c26383a9984a7433ecc37e5828
Bysbobo <boixpierre@gmail.com>
2017-12-27 00:10:18 +0100

    All the singleton modification placed into header file

--- include/singleton.h
+++ include/singleton.h
@@ -7,22 +7,48 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
+#include <iostream>
+
+//template <typename T>
 class Singleton
 {
 private:
-	Singleton();
-	~Singleton();
+	Singleton() {};
+	~Singleton() {};
 	Singleton(const Singleton&) = delete;
 	Singleton& operator=(const Singleton&) = delete;
 
 
 public:
-	static Singleton* getInstance();
-	static void kill();
+	static Singleton* getInstance()
+	{
+		if (apInstance == NULL)
+		{
+			std::cout << "Creating singleton" << std::endl;
+			apInstance = new Singleton();
+		}
+		else
+			std::cout << "Singleton already created..." << std::endl;
+
+		return apInstance;
+	}
+	
+	static void kill()
+	{
+		if (apInstance != NULL)
+		{
+			delete apInstance;
+			apInstance = NULL;
+
+			std::cout << "Singleton deleted..." << std::endl;
+		}
+	}
 
 
 private:
 	static Singleton* apInstance;
 };
+
+Singleton* Singleton::apInstance = NULL;
 
 #endif // SINGLETON_H
